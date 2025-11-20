ff0587a1c1db7a9ebc86d5f94020fac1eb62a114
Nhatto20 <japanramenazv2@gmail.com>
2025-11-21 00:58:04 +0700

    new run.sh conf for running on colab

--- examples/libritts/cosyvoice2/run.sh
+++ examples/libritts/cosyvoice2/run.sh
@@ -4,8 +4,8 @@ source ./path.sh || exit 1
 #. ./path.sh || exit 1;
 
 
-stage=3
-stop_stage=3
+stage=5
+stop_stage=7
 
 data_dir=/mnt/c/Users/japan/datasets/Speech/JVS
 pretrained_model_dir=../../../pretrained_models/JP-CosyVoice2-0.5B
@@ -52,7 +52,7 @@ if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ]; then
   for x in train test; do
     mkdir -p data/$x/parquet
     python3 tools/make_parquet_list.py --num_utts_per_parquet 1000 \
-      --num_processes 12 \
+      --num_processes 4 \
       --src_dir data/$x \
       --des_dir data/$x/parquet
   done
@@ -78,7 +78,7 @@ if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ]; then
   # cat data/viVoice-test/parquet/data.list > data/dev.data.list
   
   # NOTE will update llm/hift training later
-  for model in llm flow hifigan; do
+  for model in llm flow; do
   #for model in hifigan; do
     torchrun --nnodes=1 --nproc_per_node=$num_gpus \
         --rdzv_id=$job_id --rdzv_backend="c10d" --rdzv_endpoint="localhost:1234" \
@@ -105,7 +105,7 @@ fi
 # average model
 average_num=5
 if [ ${stage} -le 6 ] && [ ${stop_stage} -ge 6 ]; then
-  for model in llm flow hifigan; do
+  for model in llm flow; do
     decode_checkpoint=`pwd`/exp/cosyvoice/$model/$train_engine/${model}.pt
     echo "do model average and final checkpoint is $decode_checkpoint"
     python3 cosyvoice/bin/average_model.py \
